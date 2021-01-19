6c8a592fa1267e8a0b776ff6afa87ed5075d7233
ned <ned@me.com>
2021-01-19 15:04:46 +0100

    fix line height in related recipes

--- src/components/recipe/RelatedRecipes.vue
+++ src/components/recipe/RelatedRecipes.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 id="related_recipes_title" class="mb-1 ml-3 ml-lg-0 mt-xl-0 mt-2 text-left">Ricette correlate</h1>
     <ul id="related_recipes_list" class="ml-lg-0 ml-3 mb-3 pl-4 mr-3 text-left">
-      <li v-for="(name, id) in related_recipes" v-bind:key="id">
+      <li v-for="(name, id) in related_recipes" v-bind:key="id" class="mb-1">
         <router-link :to="'/recipe/' + id">{{ name }}</router-link>
       </li>
     </ul>
@@ -24,6 +24,7 @@ export default {
 
 #related_recipes_list {
   font-size: 27px;
+  line-height: 1.3;
 }
 
 </style>
